65531e7a069f8238d60b7fb14a4959bbc4c6bc0f
Sina Ghaffari <Sinaghafari97@gmail.com>
2026-08-14 01:08:46 +0330

    fix bug test

--- components/ui/tests/Carousel.test.tsx
+++ components/ui/tests/Carousel.test.tsx
@@ -10,7 +10,7 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from './Carousel';
+} from '@/components/ui/carousel';
 
 const mockEmblaApi = {
   canScrollPrev: vi.fn(() => true),
